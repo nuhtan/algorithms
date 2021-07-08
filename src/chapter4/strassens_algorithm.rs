f9f57b5fa3a51efceef43e06b535566e42b1a921
nuhtan <ncrichardson15@gmail.com>
2021-07-08 19:23:44 -0400

    added square matrix multiplication

--- src/chapter4/strassens_algorithm.rs
+++ src/chapter4/strassens_algorithm.rs
@@ -1,3 +1,15 @@
-fn square_matrix_multiply<T>(A: Vec<Vec<T>>, B: Vec<Vec<T>>) -> Vec<Vec<T>> {
-    let n = A
+fn square_matrix_multiply(a: Vec<Vec<i32>>, b: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let n = a.len();
+    let mut c: Vec<Vec<i32>> = Vec::new();
+    for i in 1..n {
+        for j in 1..n {
+            c[i][j] = 0;
+            for k in 1..n {
+                c[i][j] = c[i][j] + a[i][k] * b[k][j];
+            }
+        }
+    }
+    c
 }
+
+// TODO Strassen Method, this is not explicitly in pseudo code
