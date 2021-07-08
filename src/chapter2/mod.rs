6f621b41c5d3a83c5512054b0b353cd246376f25
nuhtan <ncrichardson15@gmail.com>
2021-07-08 19:25:49 -0400

    documentation

--- src/chapter2/mod.rs
+++ src/chapter2/mod.rs
@@ -1,1 +1,4 @@
-mod insertion_sort;
+//! # Chapter 2: Getting Started
+//! This chapter is mostly explanation on how the book will explain things and how to analyze and understand algorithms. This chapter only features a single algorithm, insertion sort.
+
+pub mod insertion_sort;
