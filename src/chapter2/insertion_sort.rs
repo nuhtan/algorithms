4c2819226efd30c89e19a5ea06b0e0a6431d0874
nuhtan <ncrichardson15@gmail.com>
2021-07-08 19:41:42 -0400

    final documentation for chapter 2?

--- src/chapter2/insertion_sort.rs
+++ src/chapter2/insertion_sort.rs
@@ -1,6 +1,12 @@
-/// Section 2.1: Insertion Sort
+/// # Section 2.1: Insertion Sort
 ///
-/// Insertion sort is an efficient sort for sorting small amounts of elements. Behaviorally it works in a similar way to how most humans sort. Insertion sort does the sorting in place and is quite simple, not requiring any helper functions or more complex operations.
+/// Insertion sort is an efficient sort for sorting small amounts of elements. Behaviorally it works in a similar way to how most humans sort. Insertion sort does the sorting in place and is quite simple, not requiring any helper functions or more complex operations or accompanying arrays.
+///
+/// A few notes:
+/// - Our loop starts at 1 as we compare the value at the index with the prior value not the following value.
+/// - This sort has a time complexity of O(n<sup>2</sup>).
+///
+/// Insertion sort loops through each value of the array starting from the front and checks if the value in front of it is of a lower value. If the value is lower, the elements are swapped, this happens until the value is at the front of the array or the value in front of it lower. This process repeats until the entire array has been traversed through the first loop.
 
 // The generic T has the Copy trait so that we can limit the types to more straight forward copyable types.
 // PartialEq and PartialOrd are included for the needed comparisons of the generic types.
--- src/chapter2/mod.rs
+++ src/chapter2/mod.rs
@@ -1,4 +1,5 @@
 //! # Chapter 2: Getting Started
 //! This chapter is mostly explanation on how the book will explain things and how to analyze and understand algorithms. This chapter only features a single algorithm, insertion sort.
 
+/// The sample algorithm used to explain concepts of pseudocode conventions and algorithmic analysis.
 pub mod insertion_sort;
