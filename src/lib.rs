dc16d5d67d76cbe258ad01795794e4a3df6a0aa1
nuhtan <ncrichardson15@gmail.com>
2021-07-09 00:12:53 -0400

    start of documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
-//! Crate Wide documentation
+#![warn(missing_docs)]
+//! # Algorithms from *Introduction to Algorithms*
+//! Text authored by Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein.
+//!
+//! This is based on the third edition.
+//!
+//! Chapters one and three are skipped as there are no specific algorithms that are detailed within them.
+
 
 pub mod chapter2;
 pub mod chapter4;
