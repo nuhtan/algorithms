cd14cdc1d61bdd4c5f488a0c99879e9b8e93f700
nuhtan <ncrichardson15@gmail.com>
2021-07-09 20:38:47 -0400

    added tests and shifted indexing

--- src/chapter2/mod.rs
+++ src/chapter2/mod.rs
@@ -3,9 +3,13 @@
 
 /// The sample algorithm used to explain concepts of pseudocode conventions and algorithmic analysis.
 pub mod insertion_sort {
+    //TODO break the lines for readability on github
     /// # Section 2.1: Insertion Sort
     ///
-    /// Insertion sort is an efficient sort for sorting small amounts of elements. Behaviorally it works in a similar way to how most humans sort. Insertion sort does the sorting in place and is quite simple, not requiring any helper functions or more complex operations or accompanying arrays.
+    /// Insertion sort is an efficient sort for sorting small amounts of elements.
+    /// Behaviorally it works in a similar way to how most humans sort. Insertion sort
+    /// does the sorting in place and is quite simple, not requiring any helper functions
+    /// or more complex operations or accompanying arrays.
     ///
     /// A few notes:
     /// - Our loop starts at 1 as we compare the value at the index with the prior value not the following value.
@@ -21,18 +25,68 @@ pub mod insertion_sort {
     ///
     /// # Returns:
     /// This method has no return type as it modifies the input in place.
-    // TODO If the function operates in place then there is no need to return the input array.
-    pub fn sort<T: Copy + PartialEq + PartialOrd>(input: &mut Vec<T>) -> &Vec<T> {
-        for j in 1..input.len() {
-            let key = input[j];
+    ///
+    /// # Special Notes:
+    /// - In and effort to follow the books usage of arrays that start at 1 al of the logic of the function has been shifted over by one.
+    pub fn sort<T: Copy + PartialEq + PartialOrd>(input: &mut Vec<T>) {
+        for j in 2..input.len() + 1 {
+            let key = input[j - 1];
             let mut i = j - 1;
-            while i > 0 && input[i] > key {
-                input[i + 1] = input[i];
+            while i > 0 && input[i - 1] > key {
+                input[i] = input[i - 1];
                 i -= 1;
             }
-            input[i + 1] = key;
+            input[i] = key;
         }
-        return input;
     }
 
+    #[cfg(test)]
+    mod tests {
+
+        /// A very basic test to ensure that an array of length one does not get modified.
+        #[test]
+        fn test_single_length_array() {
+            let mut vec = vec![5];            super::sort(&mut vec);
+            assert_eq!(vec, vec![5]);
+        }
+
+        #[test]
+        fn test_sorted_array() {
+            let mut vec = vec![1, 3, 6, 7, 45, 87];
+            let target = vec![1, 3, 6, 7, 45, 87];
+            super::sort(&mut vec);
+            assert_eq!(vec, target);
+        }
+
+        #[test]
+        fn test_reverse_sorted_array() {
+            let mut vec = vec![754, 45, 23, 22, 5];
+            let target = vec![5, 22, 23, 45, 754];
+            super::sort(&mut vec);
+            assert_eq!(vec, target);
+        }
+
+        #[test]
+        fn test_array_with_duplicates() {
+            let mut vec = vec![5, 45, 22, 22, 5];
+            let target = vec![5, 5, 22, 22, 45];
+            super::sort(&mut vec);
+            assert_eq!(vec, target);
+        }
+
+        /// This test generates 10 random arrays and sorts them, the sort is compared against the std sort for vectors.
+        #[test]
+        fn test_random_array() {
+            use rand::Rng;
+            for _ in 0..10 {
+                let mut arr = [0; 20];
+                rand::thread_rng().fill(&mut arr);
+                let mut vec = arr.to_vec();
+                let mut target = vec.clone();
+                super::sort(&mut vec);
+                target.sort();
+                assert_eq!(vec, target);
+            }
+        }
+    }
 }
